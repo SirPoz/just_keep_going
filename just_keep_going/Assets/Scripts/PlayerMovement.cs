270fb5c6402e3af9f4b7ea7dca380cad81a90ea5
straussdave <davidstrauss002@gmail.com>
2022-04-18 19:53:19 +0200

    Animations
    Animations for Idle, Walk, Attack, Air Attack, Roll, Jump and Fall added.

--- just_keep_going/Assets/Scripts/PlayerMovement.cs
+++ just_keep_going/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator anim;
 
     public float moveSpeed = 25f;
     public float acceleration = 800f;
@@ -20,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     private bool grounded = true;
     private float gravityScale;
     private float nextDashTime;
+    private bool isFacingRight = true;
 
     void Start(){
         rb = GetComponent<Rigidbody2D>(); //Der Rigidbody der dem Spieler zugeordnet ist
@@ -27,18 +29,35 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    void moveSideways(){
+    void MoveSideways(){
         float targetSpeed = Input.GetAxisRaw("Horizontal") * moveSpeed; // 1 bzw -1 * moveSpeed der in der Engine mittels Eingabe bestimmt wird
         float speedDiff = targetSpeed - rb.velocity.x;
         float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration; // Wenn der Knopf zum bewegen gedrückt ist ist accelRate = acceleration, sonst decceleration
         horizontalMove = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, velPower) * Mathf.Sign(speedDiff); 
-        rb.AddForce(horizontalMove * Vector2.right * Time.deltaTime, 0); 
+        rb.AddForce(horizontalMove * Vector2.right * Time.deltaTime, 0);
+        Flip();
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
     }
 
-    void jump(){
+    void Flip(){
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            isFacingRight = false;
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            isFacingRight = true;
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    void Jump(){
         if(Input.GetButtonDown("Jump") && jumpCounter <= 1){ 
             jumpCounter++;
-            if(rb.velocity.y < 0){ //da die anziehungskraft beim nach unten fliegen höher ist, müssen wir die jumpForce erhöhen um dagegenzuwirken
+            anim.SetBool("isJumping", true);
+            anim.SetBool("isFalling", false);
+            if (rb.velocity.y < 0){ //da die anziehungskraft beim nach unten fliegen höher ist, müssen wir die jumpForce erhöhen um dagegenzuwirken
                 rb.AddForce(new Vector2(0, jumpForce * Mathf.Abs(rb.velocity.y/15)), ForceMode2D.Impulse);
                 rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
@@ -49,21 +68,39 @@ public class PlayerMovement : MonoBehaviour
         }
 
         if(rb.velocity.y < 0){
-            rb.gravityScale = gravityScale * fallGravityMultiplier; // Dadurch fühlt sich das spiel nicht so "floaty" an, beim hinunterfliegen wird die schwerkraft erhöht
+           // rb.gravityScale = gravityScale * fallGravityMultiplier; // Dadurch fühlt sich das spiel nicht so "floaty" an, beim hinunterfliegen wird die schwerkraft erhöht
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", true);
+            anim.SetFloat("yVelocity", -1);
+        }
+        else if(rb.velocity.y > 0){
+            rb.gravityScale = gravityScale; //Wenn der spieler nicht nach unten fliegt, wird die Anziehungskraft des rigidbody wieder auf die normale gravityscale zurückgesetzt
+            anim.SetFloat("yVelocity", 1);
         }
         else{
-            rb.gravityScale = gravityScale; //Wenn der spieler nicht nach unten fliegt, wird die Anziehungskraft des rigidbody wieder auf die normale gravityscale zurückgesetzt
+            anim.SetFloat("yVelocity", 0);
         }
 
         if(grounded){
             jumpCounter = 0;
+            anim.SetBool("isFalling", false);
+            anim.SetBool("isJumping", false);
         }
     }
 
-    void dash(){
+    void Dash(){
         if(Input.GetButtonDown("Fire1") && nextDashTime <= Time.time){
+            anim.ResetTrigger("Roll");
+            anim.SetTrigger("Roll");
             rb.AddForce(dashForce * Input.GetAxisRaw("Horizontal") * Vector2.right);
            nextDashTime = Time.time + dashCooldown;
+        }
+    }
+
+    void Attack(){
+        if (Input.GetMouseButtonDown(0)){
+            anim.ResetTrigger("Attack");
+            anim.SetTrigger("Attack");
         }
     }
 
@@ -73,11 +110,14 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    
+
     void Update(){
 
-       moveSideways();
-       jump();
-       dash();
+       MoveSideways();
+       Jump();
+       Dash();
+       Attack();
 
     }
 
