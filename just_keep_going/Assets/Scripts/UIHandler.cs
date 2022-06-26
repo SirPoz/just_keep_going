d1e49971e47d8462d70fba5d94a444a11998a05f
straussdave <101413861+straussdave@users.noreply.github.com>
2022-06-26 18:30:16 +0200

    Szenenübergänge überarbeitet.
    Die Szenenübergänge funktionieren jetzt. Die Kamera ist jetzt in jeder Szene das MainCamera prefab.

--- just_keep_going/Assets/Scripts/UIHandler.cs
+++ just_keep_going/Assets/Scripts/UIHandler.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class UIHandler : MonoBehaviour
 {
-    public Slider HealthBar;
-    public Slider EnergyBar;
+    /*public Slider HealthBar;
+    public Slider EnergyBar;*/
 
     public GameObject Player;
     public GameObject upgradeMenuUI;
+    public GameObject portal;
 
-
-    public Text txt;
+    /*public Text txt;
     
-    public int RoomCount;
+    public int RoomCount;*/
 
     /*public Text TimerText; 
     public bool playing;
@@ -22,33 +22,38 @@ public class UIHandler : MonoBehaviour
 
     
     void Start(){
-
+        if (Player == null)
+        {
+            Player = GameObject.FindWithTag("Player");
+        }
+        if (portal == null)
+        {
+            portal = GameObject.FindWithTag("Portal");
+        }
         upgradeMenuUI.SetActive(false);
-        RoomCount = 1;
-        txt.text = "Rooms: " + RoomCount;
+        /*RoomCount = 1;
+        txt.text = "Rooms: " + RoomCount;*/
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Player == null){
-            Player = GameObject.FindWithTag("Player");
-        }
-        else{
+
+        /*else{
 
             int health = (int) (Player.GetComponent<PlayerEnergyHealth>().getCurrentHealth() / Player.GetComponent<PlayerEnergyHealth>().getMaxHealth()) * 7;
             HealthBar.GetComponent<BarScript>().setValue(health);
 
             int energy = (int) (Player.GetComponent<PlayerEnergyHealth>().getCurrentEnergy() / Player.GetComponent<PlayerEnergyHealth>().getMaxEnergy()) * 7;
             EnergyBar.GetComponent<BarScript>().setValue(energy);
-        }
+    }*/
 
-      /*  Timer += Time.deltaTime;
-	    int minutes = Mathf.FloorToInt(Timer / 60F);
-	    int seconds = Mathf.FloorToInt(Timer % 60F);
-	    int milliseconds = Mathf.FloorToInt((Timer * 100F) % 100F);
-	    TimerText.text = minutes.ToString ("00") + ":" + seconds.ToString ("00") + ":" + milliseconds.ToString("00");*/
+        /*  Timer += Time.deltaTime;
+          int minutes = Mathf.FloorToInt(Timer / 60F);
+          int seconds = Mathf.FloorToInt(Timer % 60F);
+          int milliseconds = Mathf.FloorToInt((Timer * 100F) % 100F);
+          TimerText.text = minutes.ToString ("00") + ":" + seconds.ToString ("00") + ":" + milliseconds.ToString("00");*/
     }
 
     public void HealthButtonClick()
@@ -56,6 +61,7 @@ public class UIHandler : MonoBehaviour
         Player.GetComponent<PlayerEnergyHealth>().setMaxHealth(Player.GetComponent<PlayerEnergyHealth>().getMaxHealth() + 50);
         upgradeMenuUI.SetActive(false);
         Time.timeScale = 1f;
+        portal.GetComponent<portalCollider>().loadNewScene();
     }
 
     public void DamageButtonClick()
@@ -63,6 +69,7 @@ public class UIHandler : MonoBehaviour
         Player.GetComponent<PlayerMovement>().attackDamage = Player.GetComponent<PlayerMovement>().attackDamage + 5;
         upgradeMenuUI.SetActive(false);
         Time.timeScale = 1f;
+        portal.GetComponent<portalCollider>().loadNewScene();
     }
 
     public void SpeedButtonClick()
@@ -70,12 +77,13 @@ public class UIHandler : MonoBehaviour
         Player.GetComponent<PlayerMovement>().moveSpeed = Player.GetComponent<PlayerMovement>().moveSpeed + 1;
         upgradeMenuUI.SetActive(false);
         Time.timeScale = 1f;
+        portal.GetComponent<portalCollider>().loadNewScene();
     }
 
     public void IncreaseRoomCount(int i = 1){
         Time.timeScale = 0f;
         upgradeMenuUI.SetActive(true);
-        RoomCount += i;
-        txt.text = "Rooms: " + RoomCount;
+        /*RoomCount += i;
+        txt.text = "Rooms: " + RoomCount;*/
     }
 }
--- just_keep_going/Assets/Scripts/portalCollider.cs
+++ just_keep_going/Assets/Scripts/portalCollider.cs
@@ -4,25 +4,28 @@ using UnityEngine.SceneManagement;
 
 public class portalCollider : MonoBehaviour
 {
-    
+    public GameObject Player;
+    public GameObject upgradeMenuUI;
     private bool activated;
     
 
     void Start(){
+        Player = GameObject.FindWithTag("Player");
         activated = false;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(!activated)
         {
+            Time.timeScale = 0f;
+            upgradeMenuUI.SetActive(true);
             Debug.Log("OnTriggerEnter2D");
             activated = true;
-            loadNewScene();
         }
    
     }
 
-    private void loadNewScene()
+    public void loadNewScene()
     {
         int i = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(i+1);
